26d5ca42e1219c2a8b69eccaa5db6057a36be242
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-07 21:13:40 +0700

    feat: landing hero sections

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,54 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "ReactJS Indonesia" },
+    {
+      name: "description",
+      content:
+        "Komunitas developer React dan React Native terbesar di Indonesia",
+    },
   ];
 };
 
 export default function Index() {
+  const heroPhotos = [
+    { slug: "hero-photo-1", text: "Photo 1", url: "" },
+    { slug: "hero-photo-2", text: "Photo 2", url: "" },
+    { slug: "hero-photo-3", text: "Photo 3", url: "" },
+    { slug: "hero-photo-4", text: "Photo 4", url: "" },
+    { slug: "hero-photo-5", text: "Photo 5", url: "" },
+  ];
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <section className="py-20 flex flex-col items-center gap-8">
+        <h1 className="text-8xl font-bold text-center text-balance">
+          <span>Komunitas Developer</span>
+          <br />
+          <span>ReactJS Indonesia</span>
+        </h1>
+        <p className="text-2xl text-slate-400 text-center text-balance max-w-6xl">
+          Bergabunglah dengan Komunitas Developer React dan React Native
+          Indonesia! Hadiri Meetup Bulanan dan dapatkan wawasan terbaru tentang
+          React dan ekosistemnya
+        </p>
+      </section>
+
+      <section>
+        <ul className="flex gap-5">
+          {heroPhotos.map((heroPhoto) => (
+            <li key={heroPhoto.slug}>
+              <img
+                src={heroPhoto.url}
+                alt={heroPhoto.text}
+                width={400}
+                height={270}
+                className="bg-slate-700 rounded-3xl"
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }

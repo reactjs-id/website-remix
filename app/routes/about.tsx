d37fdc56dd05173b8144086f0ca268c3dd72c0fb
fikrialwan <fikriar93@gmail.com>
2024-07-08 19:58:47 +0700

    feat: complete tentang kami style

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -5,10 +5,10 @@ import { MemberIcon } from "~/components/icons/member-icon";
 export default function About() {
   return (
     <div className="flex justify-center py-14">
-      <section className="max-w-7xl w-full px-16 text-[#C7CBD1] flex flex-col gap-10">
+      <section className="max-w-7xl w-full px-16 text-[#C7CBD1] flex flex-col gap-10 md:gap-12 md:text-3xl">
         <div className="flex flex-col gap-2">
-          <h1 className="text-white font-bold text-4xl">
-            {/* TODO LOGO */}
+          <h1 className="text-white font-bold text-4xl md:text-6xl">
+            <img src="/images/logos/reactjsid.svg" alt="React JS ID" className="w-16 h-9 object-contain object-left" />
             Tentang Kami
           </h1>
           <p>
@@ -22,22 +22,22 @@ export default function About() {
           mendukung, dan menginspirasi para developer dari berbagai latar
           belakang dan tingkat pengalaman melalui berbagai kegiatan dan acara.
         </p>
-        <div className="flex flex-col md:flex-row md:max-w-xl w-full justify-between gap-8">
+        <div className="flex flex-col md:flex-row md:max-w-4xl w-full justify-between gap-8">
           <div className="flex flex-col gap-1">
-            <MemberIcon width={28} height={28} />
-            <p className="font-medium text-xl text-white">100.000+</p>
+            <MemberIcon width={46} height={46} />
+            <p className="font-medium text-xl md:text-4xl text-white">100.000+</p>
             <p>Member telegram</p>
           </div>
           <div className="md:w-[1px] h-[1px] w-full md:h-full bg-[#3E4F6B]" />
           <div className="flex flex-col gap-1">
-            <EventsIcon width={28} height={28} />
-            <p className="font-medium text-xl text-white">29 Events</p>
+            <EventsIcon width={46} height={46} />
+            <p className="font-medium text-xl md:text-4xl text-white">29 Events</p>
             <p>Acara diselenggarakan</p>
           </div>
           <div className="md:w-[1px] h-[1px] w-full md:h-full bg-[#3E4F6B]" />
           <div className="flex flex-col gap-1">
-            <ArticleIcon width={28} height={28} />
-            <p className="font-medium text-xl text-white">362 Articles</p>
+            <ArticleIcon width={46} height={46} />
+            <p className="font-medium text-xl md:text-4xl text-white">362 Articles</p>
             <p>Artikel tertulis</p>
           </div>
         </div>

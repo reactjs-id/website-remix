0ea42b1d7aa1eb66290d51965305d83f0f56b107
Muhammad Hasbi <hasbi@akasia.id>
2024-07-09 08:16:49 +0800

    Site: Footer

--- app/components/shared/site-footer.tsx
+++ app/components/shared/site-footer.tsx
@@ -1,12 +1,38 @@
+import { siFacebook, siGithub, siTelegram, siX } from 'simple-icons';
+import { Navigation } from "./navigation";
+
 export function SiteFooter() {
   return (
-    <footer>
-      <span>FOOTER</span>
-      <p>
-        ReactJS Indonesia adalah komunitas developer React dan React Native
-        terbesar di Indonesia, bertujuan untuk menghubungkan dan mendukung para
-        developer.
-      </p>
+    <footer className="container flex-col mx-auto gap-14">
+      <div className="flex items-center justify-between">
+        <img className="w-[67.44px] h-[31.57px]" src="./images/logos/reactjsid.svg" alt="reactjsid-logo" />
+        <Navigation className="gap-16" />
+        <div className="flex items-center gap-8">
+          {[siFacebook, siGithub, siTelegram, siX].map(({ title, path }) => <svg key={title}
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill='white'
+            width={25}
+            height={25}
+          >
+            <title>{title}</title>
+            <path d={path} />
+          </svg>
+          )}
+        </div>
+      </div>
+      <div className="flex items-end justify-between">
+        <div className="max-w-[974px] space-y-2">
+          <h3 className="text-xl font-bold">ReactJS ID</h3>
+          <p className="text-balance">
+            ReactJS Indonesia adalah komunitas developer React dan React Native
+            terbesar di Indonesia, bertujuan untuk menghubungkan dan mendukung para
+            developer.
+          </p>
+        </div>
+        <span className="text-sm">{'\u00A9'}2024 reactjs.id</span>
+      </div>
     </footer>
   );
 }
--- app/components/shared/site-navigation.tsx
+++ app/components/shared/site-navigation.tsx
@@ -1,30 +1,14 @@
 import { Link } from "@remix-run/react";
+import { Navigation } from "./navigation";
 
 export function SiteNavigation() {
-  const navigationItems = [
-    { path: "/about", text: "Tentang Kami" },
-    { path: "/events", text: "Acara" },
-    { path: "/chapters", text: "Chapters" },
-    { path: "/resources", text: "Materi" },
-    { path: "/merchandise", text: "Merchandise" },
-    { path: "/blog", text: "Blog" },
-  ];
-
   return (
     <div className="flex justify-center items-center">
-      <nav className="flex justify-between p-10 w-full max-w-7xl">
-        <div>
-          <Link to="/">
-            <span className="text-xl font-bold">ReactJS ID</span>
-          </Link>
-        </div>
-        <ul className="flex gap-10 text-slate-400">
-          {navigationItems.map((navItem) => (
-            <li key={navItem.path}>
-              <Link to={navItem.path}>{navItem.text}</Link>
-            </li>
-          ))}
-        </ul>
+      <nav className="container">
+        <Link to="/">
+          <span className="text-xl font-bold">ReactJS ID</span>
+        </Link>
+        <Navigation className="gap-[75px]" />
       </nav>
     </div>
   );

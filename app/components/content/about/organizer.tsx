abaa33ddcf2dae283b51e61d9f9b8f0a05ccc89b
fikrialwan <fikriar93@gmail.com>
2024-07-09 22:54:22 +0700

    feat: adjustment font for mobie

--- app/components/content/about/organizer.tsx
+++ app/components/content/about/organizer.tsx
@@ -5,8 +5,10 @@ import type { OrganizerType } from "~/types/about";
 export const AboutOrganizer = () => {
   return (
     <section className="max-w-7xl w-full px-16 text-[#C7CBD1] flex flex-col gap-10 md:gap-12 md:text-3xl">
-      <h2 className="font-semibold text-4xl text-white">Pengurus</h2>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <h2 className="font-semibold text-2xl md:text-4xl text-white">
+        Pengurus
+      </h2>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
         {organizerData.map(({ fullName, role, image }, index) => (
           <li key={fullName + index}>
             <OrganizerCard fullName={fullName} image={image} role={role} />
@@ -24,7 +26,7 @@ const OrganizerCard = ({ fullName, image, role }: OrganizerType) => {
         <AvatarImage src={image} alt={`${fullName}'s profile`} />
         <AvatarFallback>{fullName[0].toUpperCase()}</AvatarFallback>
       </Avatar>
-      <div className="flex flex-col gap-[2px] font-medium text-2xl">
+      <div className="flex flex-col gap-[2px] font-medium md:text-2xl">
         <h3>{fullName}</h3>
         <p className="text-[#C7CBD1]/40">{role}</p>
       </div>

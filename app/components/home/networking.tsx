3a068078924f114ff7568d6075bf514be43cd0e1
Hani Husamuddin <hani.husam@gmail.com>
2024-07-09 09:29:35 +0700

    feat: init networking section

---
+++ app/components/home/networking.tsx
@@ -0,0 +1,13 @@
+export default function Networking() {
+  return (
+    <section className="container">
+      <div className="flex flex-col space-y-[88px] px-[43px]">
+        <div className="flex grow self-stretch">
+          <div className="flex flex-col gap-[42px]">
+            <h2 className="text-6xl font-semibold text-balance">Networking</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}

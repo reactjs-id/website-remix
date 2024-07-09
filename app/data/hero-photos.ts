b7811eb445c8d5144a0047435a262f4a828e2969
andi hakim arif <andi.hakim.arif@gmail.com>
2024-07-10 00:17:45 +0700

    feat: hero details & adjust breakpoint (#15)
    * feat: hero details & adjust breakpoint
    * fix: apply CR feedback
    * fix: use reactjsid logo

---
+++ app/data/hero-photos.ts
@@ -0,0 +1,29 @@
+const heroPhotos = [
+  {
+    slug: "hero-photo-1",
+    text: "Photo 1",
+    url: "https://placehold.co/398x268",
+  },
+  {
+    slug: "hero-photo-2",
+    text: "Photo 2",
+    url: "https://placehold.co/398x268",
+  },
+  {
+    slug: "hero-photo-3",
+    text: "Photo 3",
+    url: "https://placehold.co/398x268",
+  },
+  {
+    slug: "hero-photo-4",
+    text: "Photo 4",
+    url: "https://placehold.co/398x268",
+  },
+  {
+    slug: "hero-photo-5",
+    text: "Photo 5",
+    url: "https://placehold.co/398x268",
+  },
+];
+
+export default heroPhotos;

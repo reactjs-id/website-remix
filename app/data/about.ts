2928bc6413b9399932ea474d4dc23fd93d13266c
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2024-07-11 01:00:44 +0700

    feat: about page: pengurus (#19)
    * chore: add avatar comp
    * feat: add pengurus section
    * feat: update naming from pengurus to organizer
    * feat: adjustment font for mobile

---
+++ app/data/about.ts
@@ -0,0 +1,49 @@
+import type { OrganizerType } from "~/types/about";
+
+export const organizerData: OrganizerType[] = [
+  {
+    fullName: "Dzaki",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "M Haidar Hanif",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "Resi Respati",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "muhfaridzia",
+    role: "panelis open source",
+    image: "-",
+  },
+  {
+    fullName: "Nara",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "Yusuf",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "Bambang",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "Alif",
+    role: "kontributor open source",
+    image: "-",
+  },
+  {
+    fullName: "Gusti",
+    role: "kontributor open source",
+    image: "-",
+  },
+];

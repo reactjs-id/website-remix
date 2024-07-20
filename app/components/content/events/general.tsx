fdafc529be5ec1f2f5d36d7a1b5ab1c589dd44eb
fikrialwan <fikriar93@gmail.com>
2024-07-20 14:29:24 +0700

    feat: add event general section

---
+++ app/components/content/events/general.tsx
@@ -0,0 +1,18 @@
+export const EventsGeneral = () => {
+  return (
+    <section className="max-w-7xl w-full px-16 text-[#C7CBD1] flex flex-col gap-6 md:text-3xl items-center">
+      <img
+        src="/images/logos/reactjsid.svg"
+        alt="React JS ID"
+        className="w-16 h-9 object-contain object-left"
+      />
+      <h1 className="text-white font-bold text-4xl md:text-6xl">
+        Acara reactjs.id
+      </h1>
+      <p>
+        ReactJS ID: Komunitas Developer React dan React Native Terbesar di
+        Indonesia
+      </p>
+    </section>
+  );
+};

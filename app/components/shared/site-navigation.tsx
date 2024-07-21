031eaed3764a78a8ba60025c461de9f2bee32607
andi hakim arif <andi.hakim.arif@gmail.com>
2024-07-21 21:08:48 +0700

    fix: header navigation responsive (#51)
    * fix: header navigation responsive
    * fix: resolve bun conflict

--- app/components/shared/site-navigation.tsx
+++ app/components/shared/site-navigation.tsx
@@ -1,14 +1,37 @@
 import { Link } from "@remix-run/react";
-import { Navigation } from "./navigation";
+import { AlignRight } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
+import { Navigation, navigationItems } from "./navigation";
 
 export function SiteNavigation() {
   return (
     <div className="flex justify-center items-center">
-      <nav className="container">
-        <Link to="/">
-          <span className="text-xl font-bold">ReactJS ID</span>
+      <nav className="container !p-4 !lg:p-8 items-center">
+        <Link to="/" className="text-xl font-bold">
+          ReactJS ID
         </Link>
-        <Navigation className="gap-[75px]" />
+        <div className="hidden lg:block">
+          <Navigation className="gap-[75px]" />
+        </div>
+        <div className="block lg:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <AlignRight />
+            </SheetTrigger>
+            <SheetContent side="right" className="bg-[#15181D] text-slate-50">
+              <ul className={`flex flex-col gap-4 text-slate-400`}>
+                {navigationItems.map((navItem) => (
+                  <li key={navItem.path}>
+                    <Link to={navItem.path}>
+                      <SheetTrigger>{navItem.text}</SheetTrigger>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </SheetContent>
+          </Sheet>
+        </div>
       </nav>
     </div>
   );

b661712cd67704e5d1e8252057310361b5f84eea
Muhammad Yusuf <akuyusuf27@gmail.com>
2024-07-21 17:22:46 +0700

    chore: configure formatter (#48)
    * chore: configure formatter & formatting code
    * ci: add workflow for pull request
    * ci: fix missing `jobs` on workflows

--- app/components/shared/navigation.tsx
+++ app/components/shared/navigation.tsx
@@ -1,21 +1,22 @@
 import { Link } from "@remix-run/react";
 
 const navigationItems = [
-    { path: "/about", text: "Tentang Kami" },
-    { path: "/blog", text: "Blog" },
-    { path: "/events", text: "Acara" },
-    { path: "/chapters", text: "Chapters" },
-    { path: "/resources", text: "Materi" },
-    { path: "/merchandise", text: "Merchandise" },
+  { path: "/about", text: "Tentang Kami" },
+  { path: "/blog", text: "Blog" },
+  { path: "/events", text: "Acara" },
+  { path: "/chapters", text: "Chapters" },
+  { path: "/resources", text: "Materi" },
+  { path: "/merchandise", text: "Merchandise" },
 ];
 
-export function Navigation({ className = '' }: { className: string }) {
-    return <ul className={`flex ${className} text-slate-400`}>
-        {navigationItems.map((navItem) => (
-            <li key={navItem.path}>
-                <Link to={navItem.path}>{navItem.text}</Link>
-            </li>
-        ))}
+export function Navigation({ className = "" }: { className: string }) {
+  return (
+    <ul className={`flex ${className} text-slate-400`}>
+      {navigationItems.map((navItem) => (
+        <li key={navItem.path}>
+          <Link to={navItem.path}>{navItem.text}</Link>
+        </li>
+      ))}
     </ul>
-
+  );
 }

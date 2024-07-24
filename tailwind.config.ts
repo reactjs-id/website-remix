8c95bd9ef1acb22c7529842b22430a64876a6825
andihaki <andi.hakim.arif@gmail.com>
2024-07-24 14:35:29 +0700

    fix: change default font to Geist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: ["class"],
@@ -13,6 +14,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['"Geist Sans"', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         lightGray: "#C7CBD1",
         darkJoinButton: "#20242B",

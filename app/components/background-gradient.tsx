dd23a50bed5eb2b5ebde4eca237bdf5033075460
andihaki <andi.hakim.arif@gmail.com>
2024-07-25 09:49:22 +0700

    fix: use new branch color

--- app/components/background-gradient.tsx
+++ app/components/background-gradient.tsx
@@ -6,8 +6,8 @@ export default function BackgroundGradient() {
       className={cn(
         "absolute left-0 right-0 -z-10 block h-16 lg:h-96 w-full",
         "bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]",
-        "from-x-blue-700 via-x-black-700/15 to-x-black-900/20",
-        "dark:from-x-blue-700 dark:via-x-black-700/15 dark:to-x-black-900/20",
+        "from-brand-blue-berry via-brand-black-washed/15 to-brand-black-aswad/20",
+        "dark:from-brand-blue-berry dark:via-brand-black-washed/15 dark:to-brand-black-aswad/20",
       )}
     />
   );

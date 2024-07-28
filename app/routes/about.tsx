bc5f574aef3020431dbd2027c7f92f4dad968d8a
Muhammad Hasbi <hasbi@akasia.id>
2024-07-28 20:13:40 +0800

    fix: put meta tags to every routes

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,4 @@
+import { MetaFunction } from "@remix-run/node";
 import { AboutCommunity } from "~/components/content/about/community";
 import { AboutContact } from "~/components/content/about/contact";
 import { AboutGeneral } from "~/components/content/about/general";
@@ -6,6 +7,35 @@ import { AboutOffer } from "~/components/content/about/offer";
 import { AboutOrganizer } from "~/components/content/about/organizer";
 import { JoinCommunity } from "~/components/join-community";
 import { Line } from "~/components/line";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "About ReactJS Indonesia - Community, Resources, & Guides" },
+    {
+      property: "og:title",
+      content: "About ReactJS Indonesia - Community, Resources, & Guides",
+    },
+    {
+      name: "description",
+      content:
+        "Welcome to ReactJS Indonesia, your go-to community for everything React. Explore resources, tutorials, and connect with fellow developers to enhance your skills.",
+    },
+    {
+      name: "keywords",
+      content:
+        "ReactJS Indonesia, React community, React tutorials, learn React, JavaScript, frontend development, about react indonesia",
+    },
+    {
+      property: "og:description",
+      content:
+        "Join ReactJS Indonesia, your go-to hub for all things ReactJS. Find resources, attend events, and connect with fellow developers to enhance your ReactJS knowledge.",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+  ];
+};
 
 export default function AboutRoute() {
   return (
--- app/routes/resources.tsx
+++ app/routes/resources.tsx
@@ -1,3 +1,34 @@
+import { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Resource ReactJS Indonesia - Tutorials, Guides, and Community" },
+    {
+      property: "og:title",
+      content: "Resource ReactJS Indonesia - Tutorials, Guides, and Community",
+    },
+    {
+      name: "description",
+      content:
+        "Discover comprehensive tutorials, guides, and community resources on ReactJS for developers in Indonesia. Stay updated with the latest trends and best practices.",
+    },
+    {
+      name: "keywords",
+      content:
+        "ReactJS, React tutorials, React guides, Indonesian React community, JavaScript frameworks, frontend development, web development, coding resources, programming tutorials",
+    },
+    {
+      property: "og:description",
+      content:
+        "Explore a wealth of resources on ReactJS, including tutorials, guides, and community discussions tailored for Indonesian developers.",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+  ];
+};
+
 export default function ResourcesRoute() {
   return (
     <div>

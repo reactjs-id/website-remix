ee45689fe8ee442806d1d8cdd35d980db61b1b41
fikrialwan <fikriar93@gmail.com>
2024-08-11 20:56:00 +0700

    chore: dayjs locale format id

--- app/root.tsx
+++ app/root.tsx
@@ -5,13 +5,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import dayjs from "dayjs";
 
 import { SiteNavigation } from "~/components/shared/site-navigation";
 import { SiteFooter } from "~/components/shared/site-footer";
 
+import "dayjs/locale/id";
 import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  dayjs.locale("id");
+
   return (
     <html lang="en">
       <head>

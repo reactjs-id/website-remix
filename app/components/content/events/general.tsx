b7c915bf8358c93ba989396f52602245d5d53853
fikrialwan <fikriar93@gmail.com>
2024-08-18 12:38:21 +0700

    fix: reactjs icon import

--- app/components/content/events/general.tsx
+++ app/components/content/events/general.tsx
@@ -3,7 +3,7 @@ export const EventGeneral = () => {
     <section className="max-w-7xl w-full px-4 lg:px-16 text-brand-gray-lavender flex flex-col gap-10 lg:gap-16 lg:text-3xl my-10 lg:my-28">
       <div className="flex flex-col items-center gap-2 lg:gap-4">
         <img
-          src="/images/logos/reactjsid.svg"
+          src="/images/reactjsid.svg"
           alt="React JS ID"
           className="w-16 h-9 object-contain object-left"
         />
